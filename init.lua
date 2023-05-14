82ca3b6bf15d09c23bb58dbd1d41aab3e8e4a9b9
notify <notify-ctrl@qq.com>
2023-05-14 18:16:44 +0800

    xujiale

--- init.lua
+++ init.lua
@@ -22,7 +22,7 @@ local n_juanlaotrig = fk.CreateTriggerSkill{
   end,
   on_refresh = function(self, event, target, player, data)
     local room = player.room
-    local mark_name = "@n_juanlao" 
+    local mark_name = "@n_juanlao"
     if event == fk.CardUseFinished then
       room:setPlayerMark(player, mark_name, data.card.name)
     else
@@ -264,6 +264,186 @@ Fk:loadTranslationTable{
   ["$n_lingxiu2"] = "都听我的！",
   ["$n_qunzhi1"] = "集思广益！",
   ["$n_qunzhi2"] = "群众的智慧是无穷的！",
+}
+
+local n_xujiale = General(extension, "n_xujiale", "qun", 4)
+local n_pengji_ac = fk.CreateActiveSkill{
+  name = "n_pengji_ac",
+  min_card_num = 1,
+  card_filter = function(self, to_select, selected)
+    return table.every(selected, function(id)
+      return Fk:getCardById(id).type ~= Fk:getCardById(to_select).type
+    end)
+  end,
+  target_filter = function() return false end,
+}
+Fk:addSkill(n_pengji_ac)
+local n_pengji = fk.CreateTriggerSkill{
+  name = "n_pengji",
+  anim_type = "drawcard",
+  events = {fk.AfterCardsMove},
+  can_trigger = function(self, event, target, player, data)
+    if not player:hasSkill(self.name) then return end
+    for _, move in ipairs(data) do
+      if move.to == player.id and move.moveReason == fk.ReasonDraw
+        and move.skillName ~= self.name then
+        return true
+      end
+    end
+  end,
+  on_cost = function(self, event, target, player, data)
+    local room = player.room
+    local result, dat = room:askForUseActiveSkill(player, "n_pengji_ac",
+      "@n_pengji", true)
+
+    if result then
+      self.cost_data = dat.cards
+      return true
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local cards = self.cost_data
+    room:setPlayerMark(player, "n_pengji_dis", cards)
+    room:throwCard(cards, self.name, player)
+    player:drawCards(#cards, self.name)
+  end,
+}
+n_xujiale:addSkill(n_pengji)
+
+local function songjiCheckMove(player, move)
+  local cards = player:getMark("n_pengji_dis")
+  local valid = {}
+  for _, info in ipairs(move.moveInfo) do
+    if table.find(cards, function(id)
+      return Fk:getCardById(id).trueName == Fk:getCardById(info.cardId).trueName
+    end) then table.insert(valid, info.cardId) end
+  end
+  return valid
+end
+
+local function songjiCanUse(player)
+  local canSlash = not player:prohibitUse(Fk:cloneCard("slash"))
+  local canPeach = not player:prohibitUse(Fk:cloneCard("peach"))
+
+  canSlash = canSlash and table.find(player.room:getOtherPlayers(player), function(p)
+    return player:inMyAttackRange(p) and
+      not player:isProhibited(p, Fk:cloneCard("slash"))
+  end)
+  canPeach = canPeach and player:isWounded()
+  return canSlash, canPeach
+end
+
+local n_songji = fk.CreateTriggerSkill{
+  name = "n_songji",
+  anim_type = "offensive",
+  events = {fk.AfterCardsMove},
+  can_trigger = function(self, event, target, player, data)
+    if not player:hasSkill(self.name) then return end
+
+    for _, move in ipairs(data) do
+      if move.skillName == "n_pengji" and move.to == player.id
+        and move.moveReason == fk.ReasonDraw then
+
+        local a, b = songjiCanUse(player)
+        if #songjiCheckMove(player, move) > 0 and (a or b) then
+          return true
+        end
+      end
+    end
+  end,
+
+  on_cost = function(self, event, target, player, data)
+    local ids
+    for _, move in ipairs(data) do
+      if move.skillName == "n_pengji" and move.to == player.id
+        and move.moveReason == fk.ReasonDraw then
+
+        ids = songjiCheckMove(player, move)
+        break
+
+      end
+    end
+
+    local room = player.room
+    local c = room:askForCard(player, 1, 1, true, self.name, true,
+      tostring(Exppattern{ id = ids }), "@n_songji")
+
+    if #c == 0 then return end
+    c = c[1]
+
+    local choices = {}
+    local s, p = songjiCanUse(player)
+    if s then table.insert(choices, "slash") end
+    if p then table.insert(choices, "peach") end
+    -- table.insert(choices, "Cancel")
+    local choice = room:askForChoice(player, choices, self.name)
+
+    if choice == "peach" then
+      self.cost_data = {
+        card = c,
+        cname = choice,
+        target = player.id,
+      }
+      return true
+    elseif choice == "slash" then
+      local targets = table.filter(room:getOtherPlayers(player), function(p)
+        return player:inMyAttackRange(p) and
+          not player:isProhibited(p, Fk:cloneCard("slash"))
+      end)
+      targets = table.map(targets, Util.IdMapper)
+      local p2 = room:askForChoosePlayers(player, targets, 1, 1, "@n_songji_slash",
+        self.name, true)
+
+      if #p2 ~= 0 then
+        self.cost_data = {
+          card = c,
+          cname = choice,
+          target = p2[1],
+        }
+        return true
+      end
+    end
+  end,
+
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    local use = {}
+    local dat = self.cost_data
+
+    use.from = player.id
+    use.tos = { { dat.target } }
+    local card = Fk:cloneCard(dat.cname)
+    card:addSubcard(dat.card)
+    card.skillName = self.name
+    use.card = card
+    use.extraUse = true
+    room:useCard(use)
+
+    player:drawCards(1, self.name)
+  end,
+}
+n_xujiale:addSkill(n_songji)
+Fk:loadTranslationTable{
+  ["n_xujiale"] = "徐嘉乐",
+  ["~n_xujiale"] = "最后一次啊，注意看啊",
+  ["#n_xujiale"] = "厨邦大师",
+  ["n_pengji"] = "烹鸡",
+  ["designer:n_xujiale"] = "穈穈哒的招来",
+  ["cv:n_xujiale"] = "徐嘉乐",
+  ["illustrator:n_xujiale"] = "视频截图",
+  ["@n_pengji"] = "烹鸡：你现在可以弃置任意张不同类型的牌，然后摸等量的牌",
+  ["~n_pengji"] = "选择要弃置的牌->点击确定",
+  [":n_pengji"] = "当你不因此法摸牌后，你可以弃置任意张类型不同的牌，然后摸等量的牌。",
+  ["$n_pengji1"] = "先用油把这个鸡淋一下啊",
+  ["$n_pengji2"] = "基本上很均匀了这个皮",
+  ["n_songji"] = "颂鸡",
+  [":n_songji"] = "当你因〖烹鸡〗而摸牌后，若摸的牌中有牌名和你弃置的牌中的一张相同，" ..
+  "你可以将这些相同的牌中的一张当做【杀】（不计入次数）/【桃】使用并摸一张牌。",
+  ["$n_songji1"] = "这个烧鸡，皮酥脆，肉滑有汁，骨都带味",
+  ["$n_songji2"] = "所以是数一数二的烧鸡！",
+  ["@n_songji"] = "颂鸡：你可以将刚才摸到的牌中的一张当【杀】或者【桃】使用",
+  ["@n_songji_slash"] = "颂鸡：你已经做出了数一数二的烧鸡，现在请选择【杀】的目标",
 }
 
 return { extension }
