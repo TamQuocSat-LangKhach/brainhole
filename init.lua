294fabbbffc6e5a9bfc39186e1c28cf933d99f17
notify <notify-ctrl@qq.com>
2023-06-03 03:00:19 +0800

    盗图王

--- init.lua
+++ init.lua
@@ -622,6 +622,52 @@ Fk:loadTranslationTable{
     "则你可以对伤害目标造成一点伤害。",
 }
 
+local n_daotuwang = General(extension, "n_daotuwang", "qun", 3)
+local n_daotu = fk.CreateTriggerSkill{
+  name = "n_daotu",
+  anim_type = "drawcard",
+  events = {fk.CardUseFinished},
+  can_trigger = function(self, event, target, player, data)
+    if player:hasSkill(self.name) then
+      local card = data.card
+      if card:isVirtual() then return false end
+      if data.from == player.id then return end
+      if player:usedSkillTimes(self.name, Player.HistoryTurn) > 0 then
+        return false
+      end
+      local tab = player:getMark("@$n_daotu")
+      if table.contains(type(tab) == "table" and tab or {}, card.name) then
+        return false
+      end
+      if player.room:getCardArea(card.id) == Card.Processing then
+        return true
+      end
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    room:obtainCard(player, data.card.id, true)
+    local mark_tab = player:getMark("@$n_daotu")
+    if type(mark_tab) ~= "table" then mark_tab = {} end
+    table.insert(mark_tab, data.card.name)
+    room:setPlayerMark(player, "@$n_daotu", mark_tab)
+  end
+}
+n_daotuwang:addSkill(n_daotu)
+Fk:loadTranslationTable{
+  ["n_daotuwang"] = "盗图王",
+  ["designer:n_daotuwang"] = "Notify",
+  ["~n_daotuwang"] = "盗图王，你.....",
+  ["illustrator:n_daotuwang"] = "网络",
+  ["#n_daotuwang"] = "人畜无害",
+  ["n_daotu"] = "盗图",
+  ["@$n_daotu"] = "盗图",
+  ["$n_daotu1"] = "此图，我怎么会错失。",
+  ["$n_daotu2"] = "你的图，现在是我的了！",
+  [":n_daotu"] = "每回合限一次，当其他角色使用的非转化且非虚拟的牌结算完成后，" ..
+    "若你没有同名的手牌，则你可以获得之。每种牌名限获得一次。",
+}
+
 local extension_card = Package("brainhole_cards", Package.CardPack)
 
 local brickSkill = fk.CreateActiveSkill{
