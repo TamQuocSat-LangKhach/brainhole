54f25718ca8bea38ba9757b573db025ed4e07327
notify <notify-ctrl@qq.com>
2025-05-26 01:32:43 +0800

    复活吧Q系列头像

--- pkg/new_story/init.lua
+++ pkg/new_story/init.lua
@@ -106,7 +106,76 @@ Fk:loadTranslationTable{
   ["illustrator:n_jz__huatuo"] = "黑羽",
 }
 
+local zhouyu = General:new(extension, "q_zhouyu", "wu", 3)
+zhouyu.total_hidden = true
+zhouyu:addSkills { "yingzi", "fanjian" }
+
+local machao = General:new(extension, "q_machao", "shu", 4)
+machao.total_hidden = true
+machao:addSkills { "mashu", "tieqi" }
+
+local sunshangxiang = General:new(extension, "q_sunshangxiang", "wu", 3, 3, General.Female)
+sunshangxiang.total_hidden = true
+sunshangxiang:addSkills { "xiaoji", "jieyin" }
+
+local daqiao = General:new(extension, "q_daqiao", "wu", 3, 3, General.Female)
+daqiao.total_hidden = true
+daqiao:addSkills { "guose", "liuli" }
+
+local xiaoqiao = General:new(extension, "q_xiaoqiao", "wu", 3, 3, General.Female)
+xiaoqiao.total_hidden = true
+xiaoqiao:addSkills { "tianxiang", "hongyan" }
+
+local luxun = General:new(extension, "q_luxun", "wu", 3)
+luxun.total_hidden = true
+luxun:addSkills { "qianxun", "lianying" }
+
+local huangyueying = General:new(extension, "q_huangyueying", "shu", 3, 3, General.Female)
+huangyueying.total_hidden = true
+huangyueying:addSkills { "jizhi", "qicai" }
+
+local spsunshangxian = General:new(extension, "q_spsunshangxiang", "shu", 3, 3, General.Female)
+spsunshangxian.total_hidden = true
+spsunshangxian:addSkills { "liangzhu", "fanxiang" }
+
+local bulianshi = General:new(extension, "q_bulianshi", "wu", 3, 3, General.Female)
+bulianshi.total_hidden = true
+bulianshi:addSkills { "anxu", "zhuiyi" }
+
+local zhangchunhua = General:new(extension, "q_zhangchunhua", "wei", 3, 3, General.Female)
+zhangchunhua.total_hidden = true
+zhangchunhua:addSkills { "jueqing", "shangshi" }
+
+local madai = General:new(extension, "q_madai", "shu", 4)
+madai.total_hidden = true
+madai:addSkills { "mashu", "nos__qianxi" }
+
+local wangyi = General:new(extension, "q_wangyi", "wei", 3, 3, General.Female)
+wangyi.total_hidden = true
+wangyi:addSkills { "nos__zhenlie", "nos__miji" }
+
+local guanyinping = General:new(extension, "q_guanyinping", "shu", 3, 3, General.Female)
+guanyinping.total_hidden = true
+guanyinping:addSkills { "xuehen", "huxiao", "wuji" }
+
+local liru = General:new(extension, "q_liru", "qun", 3)
+liru.total_hidden = true
+liru:addSkills { "ol_ex__juece", "ol_ex__mieji", "fencheng" }
+
+local caiwenji = General:new(extension, "q_caiwenji", "qun", 3, 3, General.Female)
+caiwenji.total_hidden = true
+caiwenji:addSkills { "beige", "duanchang" }
+
+local spdiaochan = General:new(extension, "q_spdiaochan", "qun", 3, 3, General.Female)
+spdiaochan.total_hidden = true
+spdiaochan:addSkills { "lihun", "biyue" }
+
+local fuhuanghou = General:new(extension, "q_fuhuanghou", "qun", 3, 3, General.Female)
+fuhuanghou.total_hidden = true
+fuhuanghou:addSkills { "zhuikong", "qiuyuan" }
+
 Fk:loadTranslationTable{
+  ["q_zhouyu"] = "Q周瑜",
   ["q_machao"] = "Q马超",
   ["q_sunshangxiang"] = "Q香香",
   ["q_daqiao"] = "Q大乔",
@@ -114,8 +183,12 @@ Fk:loadTranslationTable{
   ["q_luxun"] = "Q陆逊",
   ["q_huangyueying"] = "Q月英",
   ["q_spsunshangxiang"] = "Q蜀香",
+  ["q_bulianshi"] = "Q步步",
+  ["q_zhangchunhua"] = "Q春哥",
+  ["q_madai"] = "Q马岱",
   ["q_wangyi"] = "Q王异",
   ["q_guanyinping"] = "Q银屏",
+  ["q_liru"] = "Q李儒",
   ["q_caiwenji"] = "Q文姬",
   ["q_spdiaochan"] = "Q貂蝉",
   ["q_fuhuanghou"] = "Q伏后",
